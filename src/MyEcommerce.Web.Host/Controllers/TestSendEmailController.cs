9dd91d36e58594bb78212e15a2c0c3a69cc839a6
Hanaa Mohammed <123514315+HanaaMohammedGad96@users.noreply.github.com>
2023-06-12 23:36:36 +0300

    try sending email

---
+++ src/MyEcommerce.Web.Host/Controllers/TestSendEmailController.cs
@@ -0,0 +1,26 @@
+﻿using Abp.AspNetCore.Mvc.Controllers;
+using Abp.Net.Mail;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace MyEcommerce.Web.Host.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TestSendEmailController : AbpController
+    {
+        private readonly IEmailSender _emailSender;
+        public TestSendEmailController(IEmailSender emailSender)
+        => _emailSender = emailSender;
+        [HttpGet]
+        public async Task<ActionResult> TestSendEmail()
+        {
+            string email = "hanaa.mohammed.gad@gmail.com";
+            string subject = "Asp.Net Boilerplate";
+            string body = $"Dear Hanaa 😍, your testing 🧪 has been successed🎇.";
+           await  _emailSender.SendAsync(email, subject, body);
+            return Ok("Hi 👋");
+        }
+    }
+}
